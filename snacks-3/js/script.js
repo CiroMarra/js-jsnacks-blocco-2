9327347059618349355d654919b379dd8cc015b0
CiroMarra <tensha93@gmail.com>
2024-03-12 19:10:01 +0100

    feat: add script to snacks-3

--- snacks-3/js/script.js
+++ snacks-3/js/script.js
@@ -1,3 +1,32 @@
 // Crea due tag div con due id diversi:
 // un div avrà il testo colorato di rosso mentre l’altro di verde.
 // Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
+
+let pari = document.getElementById('pari');
+    pari.classList.add('red');
+
+let dispari = document.getElementById('dispari');
+    dispari.classList.add('green');    
+
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+const even = [];
+const odd = [];
+
+for (let i=0; i < numbers.length; i++ ) {
+    if (numbers[i] % 2 ===0 ) {
+        even.push(numbers[i]);
+    } else {
+        odd.push(numbers[i]);
+    }
+}
+console.log(even);
+console.log(odd);
+// pari.innerHTML = even.join(" ");
+// dispari.innerHTML = odd.join(" ");
+
+document.getElementById('pari').innerHTML = even.join (" ")
+document.getElementById('dispari').innerHTML = odd.join (" ")
+
+
+
+
